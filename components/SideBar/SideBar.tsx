4b52a9f683861245852b0df2f5a7697776c00fd6
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-11-30 23:46:40 +0100

    Render onClick modal for adding new channel section

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -29,7 +29,7 @@ const SideBar = () => {
   return (
     <div className='fixed top-0 left-0 h-screen w-16 flex flex-col bg-white dark:bg-gray-900 shadow-lg'>
       <SideBarIcon icon={<FireIcon className='w-20 h-20' />} text='Chatify' />
-      <Divider />
+      <hr className='sidebar-hr' />
       <SideBarIcon
         icon={
           <PlusIcon
@@ -43,7 +43,7 @@ const SideBar = () => {
         icon={<CogIcon className='w-9 h-9 text-blue-200' />}
         text='Settings'
       />
-      <Divider />
+      <hr className='sidebar-hr' />
       <ThemeSwitch />
 
       <Modal
@@ -64,7 +64,5 @@ const SideBarIcon = ({ icon, text }: { icon: JSX.Element; text: string }) => (
     <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
   </div>
 )
-
-const Divider = () => <hr className='sidebar-hr' />
 
 export default SideBar
