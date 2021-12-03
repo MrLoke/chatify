7feee6c8fba9e5fcdf6e81a852792bdf3f612142
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-03 22:45:27 +0100

    Create single message component

---
+++ components/ChatMessage/ChatMessage.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import Image from 'next/image'
+import { DocumentData } from '@firebase/firestore'
+import formatDistance from 'date-fns/formatDistance'
+
+const ChatMessage = ({ data }: { data: DocumentData }) => {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  return (
+    <div className='post'>
+      <div className='avatar-wrapper'>
+        <Image
+          src={data.profileImage}
+          alt={`${data.userName} avatar`}
+          layout='fill'
+          objectFit='cover'
+          className='rounded-full'
+        />
+      </div>
+
+      <div className='post-content relative'>
+        <p className='post-owner'>
+          {data.userName}
+          <small className='timestamp'>
+            {data.timestamp
+              ? formatDistance(
+                  new Date(data.timestamp.seconds * 1000),
+                  new Date(),
+                  {
+                    addSuffix: true,
+                  }
+                )
+              : null}
+          </small>
+        </p>
+        <p className='post-text'>{data.message}</p>
+        {data.image ? (
+          <div className='relatives'>
+            <Image
+              src={data.image}
+              alt={`Image post by ${data.userName}`}
+              priority
+              onLoadingComplete={(e) => {
+                const ratio = Math.min(
+                  300 / e.naturalWidth,
+                  500 / e.naturalHeight
+                )
+                setWidth(e.naturalWidth * ratio)
+                setHeight(e.naturalHeight * ratio)
+              }}
+              width={width}
+              height={height}
+            />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default ChatMessage
