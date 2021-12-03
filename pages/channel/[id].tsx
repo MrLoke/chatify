9f569bf0f9512b7da5403cbf03c07524f4dd0e17
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-03 22:49:15 +0100

    Fetch messages from firestore on server side

--- pages/channel/[id].tsx
+++ pages/channel/[id].tsx
@@ -3,13 +3,38 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import ChannelBar from 'components/ChannelBar/ChannelBar'
-import ContentContainer from 'components/ContentContainer/ContentContainer'
+import ChatFeed from 'components/ChatFeed/ChatFeed'
 import SideBar from 'components/SideBar/SideBar'
 import { collection, getDocs } from '@firebase/firestore'
-import { db } from 'firebase-config'
+import { auth, db } from 'firebase-config'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import SignInForm from 'components/SignInForm/SignInForm'
 
-const Channel: NextPage = () => {
+const Channel: NextPage = ({
+  messages,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [user, loading, error] = useAuthState(auth)
   const router = useRouter()
+
+  if (loading) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-t-2 border-blue-200'></div>
+      </div>
+    )
+  }
+
+  if (!user) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <SignInForm />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <div className='flex'>{error}</div>
+  }
 
   return (
     <>
@@ -22,28 +47,30 @@ const Channel: NextPage = () => {
       <div className='flex h-screen'>
         <SideBar />
         <ChannelBar />
-        <ContentContainer />
+        <ChatFeed messages={messages} />
       </div>
     </>
   )
 }
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   console.log(context)
-//   const channels: any = []
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const messages: { id: string }[] = []
 
-//   const querySnapshot = await getDocs(collection(db, 'channels'))
+  const querySnapshot = await getDocs(
+    //@ts-ignore
+    collection(db, 'channels', context?.params?.id, 'messages')
+  )
 
-//   querySnapshot.forEach((doc) => {
-//     channels.push({
-//       ...doc.data(),
-//       id: doc.id,
-//     })
-//   })
+  querySnapshot.forEach((doc) => {
+    messages.push({
+      ...doc.data(),
+      id: doc.id,
+    })
+  })
 
-//   return {
-//     props: { channels },
-//   }
-// }
+  return {
+    props: { messages: JSON.stringify(messages) },
+  }
+}
 
 export default Channel
