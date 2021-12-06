6f13d84d7cf71d6c1159d45111f1f2b6bae08c12
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-06 20:55:20 +0100

    Add emoji picker

--- components/ChatTextarea/ChatTextarea.tsx
+++ components/ChatTextarea/ChatTextarea.tsx
@@ -1,16 +1,26 @@
-import { ChangeEvent, FormEvent, useCallback, useState } from 'react'
+import { ChangeEvent, FormEvent, useCallback, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
-import { PlusCircleIcon, ArrowSmRightIcon } from '@heroicons/react/solid'
+import {
+  PlusCircleIcon,
+  ArrowSmRightIcon,
+  EmojiHappyIcon,
+} from '@heroicons/react/solid'
 import { addDoc, collection, serverTimestamp } from '@firebase/firestore'
 import { getDownloadURL, ref, uploadBytes } from '@firebase/storage'
 import { auth, db, storage } from 'firebase-config'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
+import useOnClickOutside from 'hooks/useOnClickOutside'
 
 const ChatTextarea = () => {
   const [user] = useAuthState(auth)
   const [message, setMessage] = useState('')
+  const [showPicker, setShowPicker] = useState(false)
   const [selectedFile, setSelectedFile] = useState<any>(null)
+  const emojiPickerRef = useRef<HTMLDivElement>(null)
   const router = useRouter()
+  useOnClickOutside(emojiPickerRef, () => setShowPicker(false))
 
   const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) =>
     setMessage(e.target.value)
@@ -62,7 +72,9 @@ const ChatTextarea = () => {
 
   return (
     <div className='bottom-bar'>
-      <form onSubmit={sendMessage} className='flex w-full'>
+      <form
+        onSubmit={sendMessage}
+        className='flex w-full relative items-center'>
         <div className='flex items-center mr-2'>
           <label htmlFor='upload-file' className='cursor-pointer'>
             <PlusCircleIcon className='text-blue-200 w-8 h-8' />
@@ -83,13 +95,37 @@ const ChatTextarea = () => {
           type='text'
           value={message}
           onChange={handleMessageChange}
-          placeholder='Enter message in'
+          placeholder={`Enter message in ${router.query.name}`}
           className='bottom-bar-input'
         />
 
         <button type='submit'>
           <ArrowSmRightIcon className='text-blue-200 w-10 h-10' />
         </button>
+
+        <div
+          className='px-2 cursor-pointer'
+          onClick={() => setShowPicker(!showPicker)}>
+          <EmojiHappyIcon className='w-7 h-7 text-yellow-300' />
+        </div>
+        {showPicker && (
+          <div ref={emojiPickerRef}>
+            <Picker
+              style={{
+                position: 'absolute',
+                bottom: '100%',
+                right: '0%',
+                cursor: 'pointer',
+              }}
+              set='twitter'
+              theme='dark'
+              onSelect={(emoji) => {
+                //@ts-ignore
+                setMessage((prevState) => prevState + emoji.native)
+              }}
+            />
+          </div>
+        )}
       </form>
     </div>
   )
