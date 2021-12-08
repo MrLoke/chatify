76a59fa75799c8baf2d2a4079bdb97005aff99d4
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-08 19:00:33 +0100

    Add default avatar if user didn't upload any

--- components/SignUpForm/SignUpForm.tsx
+++ components/SignUpForm/SignUpForm.tsx
@@ -48,34 +48,55 @@ const SignUpForm = () => {
     setLoading(true)
     createUserWithEmailAndPassword(auth, email, password)
       .then(async ({ user }) => {
-        const imageRef = ref(storage, `users/${user.uid}/${avatar.name}`)
+        // Logic about user avatar
+        if (avatar) {
+          const imageRef = ref(storage, `users/${user.uid}/${avatar.name}`)
 
-        await uploadBytes(imageRef, avatar).then(async (snapshot) => {
-          const downloadURL = await getDownloadURL(imageRef)
+          await uploadBytes(imageRef, avatar).then(async (snapshot) => {
+            const downloadURL = await getDownloadURL(imageRef)
 
-          await updateProfile(user, {
-            displayName,
-            photoURL: downloadURL,
+            await updateProfile(user, {
+              displayName,
+              photoURL: downloadURL,
+            })
+            // Add new user to firestore db
+            const docRef = doc(db, 'users', user.uid)
+            await setDoc(docRef, {
+              email,
+              displayName,
+              avatar: downloadURL,
+              status: user.metadata.lastSignInTime,
+              created: user.metadata.creationTime,
+              uid: user.uid,
+            })
           })
-
-          // Add new user to firestore db
-          const docRef = doc(db, 'users', user.uid)
-          await setDoc(docRef, {
-            email,
-            displayName,
-            avatar: downloadURL,
-            status: user.metadata.lastSignInTime,
-            created: user.metadata.creationTime,
-            uid: user.uid,
+        } else {
+          const defaultAvatar = ref(storage, `users/default_avatar.png`)
+          await getDownloadURL(defaultAvatar).then(async (imgUrl) => {
+            await updateProfile(user, {
+              displayName,
+              photoURL: imgUrl,
+            })
+            // Add new user to firestore db
+            const docRef = doc(db, 'users', user.uid)
+            await setDoc(docRef, {
+              email,
+              displayName,
+              avatar: imgUrl,
+              status: user.metadata.lastSignInTime,
+              created: user.metadata.creationTime,
+              uid: user.uid,
+            })
           })
-        })
+        }
 
         // Redirect user to chat page after successfully sign up
         router.push('/')
       })
       .catch((error) => {
         setLoading(false)
-        setIsError(error)
+        setIsError(error.message)
+        console.log(error)
       })
   }
 
@@ -177,7 +198,7 @@ const SignUpForm = () => {
               validate: (value) =>
                 value === password.current || 'The passwords do not match',
             })}
-            type='password'
+            type={showPassword ? 'text' : 'password'}
             placeholder='Confirm password'
             name='confirmPassword'
             className='input'
@@ -225,7 +246,23 @@ const SignUpForm = () => {
           className='submit-btn flex justify-center'
           disabled={loading}>
           {loading ? (
-            <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-t-2 border-gray-200'></div>
+            <svg
+              className='animate-spin h-7 w-7 text-white'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'>
+              <circle
+                className='opacity-25'
+                cx='12'
+                cy='12'
+                r='10'
+                stroke='#fff'
+                strokeWidth='4'></circle>
+              <path
+                className='opacity-75'
+                fill='#fff'
+                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+            </svg>
           ) : (
             <p>Sign Up</p>
           )}
