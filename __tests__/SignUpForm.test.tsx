b5144ebb048cc7b04befbe495f64d296f6facff9
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-10 19:46:36 +0100

    Test sign up form validation

---
+++ __tests__/SignUpForm.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import SignUpForm from 'components/SignUpForm/SignUpForm'
+
+const mockSignUp = jest.fn((email, password) => Promise.resolve)
+
+describe('SignUpForm validation', () => {
+  it('Should display error message if inputs fields is empty', async () => {
+    render(<SignUpForm />)
+
+    await act(async () => {
+      userEvent.click(screen.getByLabelText('sign-up-button'))
+    })
+
+    expect(screen.getByText(/Username is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/Email is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/Password is required/i)).toBeInTheDocument()
+    expect(mockSignUp).not.toHaveBeenCalled()
+  })
+
+  it('Should display error message if email is invalid', async () => {
+    render(<SignUpForm />)
+
+    userEvent.type(screen.getByLabelText('email-input'), 'email-input')
+    await act(async () => {
+      userEvent.click(screen.getByLabelText('sign-up-button'))
+    })
+
+    expect(screen.getByText(/Invalid e-mail address/i)).toBeInTheDocument()
+    expect(mockSignUp).not.toHaveBeenCalled()
+  })
+})
+
+it('Should be called log in when email and password are valid', async () => {
+  mockSignUp('demo@gmail.com', 'Demo123')
+  render(<SignUpForm />)
+
+  userEvent.type(screen.getByLabelText('email-input'), 'demo@gmail.com')
+  userEvent.type(screen.getByLabelText('password-input'), 'Demo123')
+
+  await act(async () => {
+    userEvent.click(screen.getByLabelText('sign-up-button'))
+  })
+
+  expect(mockSignUp).toHaveBeenCalled()
+  expect(mockSignUp).toHaveBeenCalledWith('demo@gmail.com', 'Demo123')
+})
