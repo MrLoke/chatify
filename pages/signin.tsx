4e480261cfee7ae62bbc3ddf60729b39c1a5c535
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-10 20:00:50 +0100

    Add redirect to home page if user is already logged in

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import SignInForm from 'components/SignInForm/SignInForm'
+import { auth } from 'firebase-config'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 const Signin: NextPage = () => {
+  const [user] = useAuthState(auth)
+  const router = useRouter()
+
+  useEffect(() => {
+    const redirect = () => {
+      if (user !== null) {
+        router.push('/')
+      }
+    }
+    redirect()
+  }, [user, router])
+
   return (
     <>
       <Head>
