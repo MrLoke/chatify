17254babe6f647fc238f83b7a210098a96e7e5cc
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-10 19:48:11 +0100

    Test sign in form validation

--- __tests__/SignInForm.test.tsx
+++ __tests__/SignInForm.test.tsx
@@ -2,16 +2,55 @@ import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import SignInForm from 'components/SignInForm/SignInForm'
 
-describe('SignInForm', () => {
-  const mockLogin = jest.fn()
+const mockLogin = jest.fn((email, password) => Promise.resolve)
+
+describe('SignInForm validation', () => {
   it('Should display error message if email field is empty', async () => {
     render(<SignInForm />)
 
     await act(async () => {
-      userEvent.click(screen.getByRole('button', { name: /Sign In/i }))
+      userEvent.click(screen.getByLabelText('sign-in-button'))
     })
 
-    expect(screen.getByText(/email is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/Email is required/i)).toBeInTheDocument()
     expect(mockLogin).not.toHaveBeenCalled()
   })
+
+  it('Should display error message if email is invalid', async () => {
+    render(<SignInForm />)
+
+    userEvent.type(screen.getByLabelText('email-input'), 'email-input')
+    await act(async () => {
+      userEvent.click(screen.getByLabelText('sign-in-button'))
+    })
+
+    expect(screen.getByText(/Invalid e-mail address/i)).toBeInTheDocument()
+    expect(mockLogin).not.toHaveBeenCalled()
+  })
+
+  it('Should display error message if password field is empty', async () => {
+    render(<SignInForm />)
+
+    await act(async () => {
+      userEvent.click(screen.getByLabelText('sign-in-button'))
+    })
+
+    expect(screen.getByText(/Password is required/i)).toBeInTheDocument()
+    expect(mockLogin).not.toHaveBeenCalled()
+  })
+})
+
+it('Should be called log in when email and password are valid', async () => {
+  mockLogin('demo@gmail.com', 'Demo123')
+  render(<SignInForm />)
+
+  userEvent.type(screen.getByLabelText('email-input'), 'demo@gmail.com')
+  userEvent.type(screen.getByLabelText('password-input'), 'Demo123')
+
+  await act(async () => {
+    userEvent.click(screen.getByLabelText('sign-in-button'))
+  })
+
+  expect(mockLogin).toHaveBeenCalled()
+  expect(mockLogin).toHaveBeenCalledWith('demo@gmail.com', 'Demo123')
 })
