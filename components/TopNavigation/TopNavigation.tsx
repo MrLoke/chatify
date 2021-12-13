4ca7cb6641148390a7d5241fe6ef58b60a34d771
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-13 23:48:45 +0100

    Add conditional header rendering based on route

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -4,7 +4,6 @@ import { HashtagIcon, BellIcon, UserCircleIcon } from '@heroicons/react/solid'
 
 const TopNavigation = () => {
   const router = useRouter()
-  console.log(router)
 
   return (
     <div className='top-navigation'>
@@ -16,6 +15,7 @@ const TopNavigation = () => {
         {router.route === '/' && 'New register users'}
         {router.route === '/channel/[id]' && router.query.name}
       </h5>
+
       <div className='hidden sm:flex'>
         <BellIcon className='w-7 h-7 top-navigation-icon' />
         <UserCircleIcon className='w-7 h-7 top-navigation-icon' />
