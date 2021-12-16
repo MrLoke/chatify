8d94a7992dc910c612703c0bdd7ac40be4bcc003
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-16 15:57:08 +0100

    Style created user

--- components/WelcomeUser/WelcomeUser.tsx
+++ components/WelcomeUser/WelcomeUser.tsx
@@ -4,8 +4,14 @@ import formatDistance from 'date-fns/formatDistance'
 
 const WelcomeUser = ({ data }: { data: DocumentData }) => {
   return (
-    <div className='post items-center flex-wrap justify-between'>
-      <div className='flex items-center'>
+    <div className='flex-col w-full my-2 py-1 px-4 md:px-8 hover:bg-gray-400 dark:hover:bg-gray-800 transition-all'>
+      <span className='text-gray-500 text-sm'>
+        Created{' '}
+        {formatDistance(new Date(data.created), new Date(), {
+          addSuffix: true,
+        })}
+      </span>
+      <div className='flex items-center flex-wrap mt-2'>
         <p className='mr-2'>Welcome</p>
         <div className='relative w-10 h-10 mr-2'>
           <Image
@@ -16,16 +22,11 @@ const WelcomeUser = ({ data }: { data: DocumentData }) => {
             className='rounded-full'
           />
         </div>
-        <p className='text-blue-200 cursor-pointer'>@{data.displayName}</p>
-        <p className='ml-2'>in Chatify!</p>
+        <p className='text-blue-200 mr-2 cursor-pointer truncate'>
+          @{data.displayName}
+        </p>
+        <p>in Chatify!</p>
       </div>
-      &nbsp;&nbsp;
-      <span className='text-gray-500 dark:text-gray-400 text-sm'>
-        Created{' '}
-        {formatDistance(new Date(data.created), new Date(), {
-          addSuffix: true,
-        })}
-      </span>
     </div>
   )
 }
