0782a06813ff7d804056027be8f70e7e3372a7ef
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-16 20:56:28 +0100

    Test channel component

---
+++ __tests__/Channel.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Channel from 'components/Channel/Channel'
+import { createMockRouter } from 'test-utils/createMockRouter'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+
+describe('Channel', () => {
+  const channelId = '9yayD50dF36qctMimCqT'
+  const channelName = 'React.js'
+
+  test('Should push router to channel after click', async () => {
+    const router = createMockRouter({
+      query: { name: 'React.js' },
+      pathname: `/channel/9yayD50dF36qctMimCqT`,
+    })
+
+    render(
+      <RouterContext.Provider value={router}>
+        <Channel id={channelId} channelName={channelName} />
+      </RouterContext.Provider>
+    )
+
+    userEvent.click(screen.getByTestId('push-to-channel'))
+    expect(router.push).toHaveBeenCalledWith({
+      pathname: '/channel/9yayD50dF36qctMimCqT',
+      query: { name: 'React.js' },
+    })
+  })
+})
