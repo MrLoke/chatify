0f685b652401c9e8ae8d8750a7c8b4ae82b2ebf2
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-12-18 17:53:09 +0100

    Use create mock router

--- __tests__/Channel.test.tsx
+++ __tests__/Channel.test.tsx
@@ -8,7 +8,7 @@ describe('Channel', () => {
   const channelId = '9yayD50dF36qctMimCqT'
   const channelName = 'React.js'
 
-  test('Should push router to channel after click', async () => {
+  it('Should push router to channel after click', async () => {
     const router = createMockRouter({
       query: { name: 'React.js' },
       pathname: `/channel/9yayD50dF36qctMimCqT`,
